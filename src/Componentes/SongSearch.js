ef775f26fe3d2dee52c102134987eab50cf594d9
Diego Vargas <alejolc90@gmail.com>
2022-02-11 21:29:21 -0500

    [ADD] llamados a las respectivas apis

--- src/Componentes/SongSearch.js
+++ src/Componentes/SongSearch.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { helpHttp } from "../helpers/helpHttp";
 import Loader from "./Loader";
 import SongDetails from "./SongDetails";
 import SongForm from "./SongForm";
@@ -8,6 +9,31 @@ const SongSearch = () => {
   const [bio, setBio] = useState(null);
   const [songLyric, setSongLyric] = useState(null);
   const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+    if (search === null) return;
+
+    const FetchData = async () => {
+      const { artist, song } = search;
+
+      let urlArtist = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=${artist}`;
+      let urlSong = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+      setLoader(true);
+
+      const [resArtist, resSong] = await Promise.all([
+        helpHttp().get(urlArtist),
+        helpHttp().get(urlSong),
+      ]);
+
+      console.log(resArtist, resSong);
+      setBio(resArtist);
+      setSongLyric(resSong);
+      setLoader(false);
+    };
+
+    FetchData();
+  }, [search]);
 
   const handlerSearch = (data) => {
     setSearch(data);
